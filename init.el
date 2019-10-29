5a651ec3507b568d288a82c53f318a19c44d7339
griffin-han <hanb302@163.com>
2019-10-29 14:27:20 +0800

    Update init.el

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.50")
  '(package-selected-packages
    (quote
     (sr-speedbar py-autopep8 flycheck jedi smartparens comment-dwim-2 avy monokai-pro-theme neotree))))
