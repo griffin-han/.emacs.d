be027a673c974645991655c4d874d8a315a84825
griffin-han <hanb302@163.com>
2019-10-30 09:56:58 +0800

    Update init.el

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; run package-install-selected-packages and then edit jedi:environment-root below
+;; run package-install-selected-packages and then edit jedi:environment-root below and uncomment (jedi:install-server), run once
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize) ;; You might already have this line
