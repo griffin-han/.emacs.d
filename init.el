001b370ab9076c438070365c1d7e3eaa92db19c9
griffin-han <hanb302@163.com>
2019-11-29 18:30:43 +0800

    Update init.el
    add evil and airline config

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;; package --- Summary
 ;; run package-install-selected-packages and then edit jedi:environment-root below and run M-x jedi:install-server run once
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
@@ -7,9 +8,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b59d7adea7873d58160d368d42828e7ac670340f11f36f67fa8071dbf957236a" default)))
  '(package-selected-packages
    (quote
-    (ace-window ein sr-speedbar py-autopep8 flycheck jedi smartparens comment-dwim-2 avy monokai-pro-theme neotree))))
+    (airline-themes evil-terminal-cursor-changer evil-nerd-commenter evil yasnippet-snippets yasnippet ace-window ein sr-speedbar py-autopep8 flycheck jedi smartparens avy monokai-pro-theme neotree))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,10 +26,10 @@
 (global-set-key (kbd "M-s n") 'neotree-toggle)
 ;;(display-line-numbers-mode 1)
 
+
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
 (global-set-key (kbd "M-s f") 'avy-goto-char)
-(global-set-key (kbd "M-s ;") 'comment-dwim-2)
 (setq electric-pair-preserve-balance nil)
 (require 'smartparens-config)
 
@@ -36,18 +40,31 @@
 (my-smartparens-mode t)
 
 ;; Standard Jedi.el setting
-(setq jedi:environment-root "/data/hanbing/.conda/envs/multimodal")
+(setq jedi:environment-root "/data/hanbing/miniconda2/envs/emacs")
 ;; (jedi:install-server)
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 ;; (setq jedi:use-shortcuts t)
+
+
+
+(require 'evil)
+(evil-mode 1)
+(global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+(require 'airline-themes)
+(load-theme 'airline-light)
+
+(require 'yasnippet)
+(yas-reload-all)
+(add-hook 'prog-mode-hook #'yas-minor-mode)
+
 
 (add-hook 'python-mode-hook (semantic-mode 1))
 (add-hook 'speedbar-load-hook (lambda () (require 'semantic/sb)))
 (global-set-key (kbd "M-s p") 'sr-speedbar-toggle)
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(global-set-key (kbd "M-s l") 'py-autopep8)
+(global-set-key (kbd "M-s l") 'py-autopep8-buffer)
 
 (global-set-key (kbd "M-RET") (lambda ()
                                 (interactive)
